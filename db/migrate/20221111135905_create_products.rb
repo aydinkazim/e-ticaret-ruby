6804298e2ff0679dbfb9018870ae26dff8c2c563
aydinkazim <kzmaydn@hotmail.com>
2022-11-11 17:00:30 +0300

    first model added

---
+++ db/migrate/20221111135905_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :color
+
+      t.timestamps
+    end
+  end
+end
