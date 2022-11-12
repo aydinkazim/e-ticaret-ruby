c31fa0806428fd851538016f6b72fa3b2cc04821
aydinkazim <kzmaydn@hotmail.com>
2022-11-12 21:27:41 +0300

    resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  get '/products', to: 'products#index'
-  get 'products/:id', to: 'products#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products
 end
