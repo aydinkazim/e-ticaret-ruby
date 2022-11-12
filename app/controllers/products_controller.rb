ef0ea6309c2a922340aadad8dc194e7a1493cf39
aydinkazim <kzmaydn@hotmail.com>
2022-11-12 21:40:37 +0300

    creating new product controller and view added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,23 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :description, :color)
+    end
 end
