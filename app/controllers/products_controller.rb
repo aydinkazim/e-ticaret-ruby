acc7fad635d7d9e18989d009e35019f9ceaa1bcf
aydinkazim <kzmaydn@hotmail.com>
2022-11-12 22:27:29 +0300

    update product controller and view added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,20 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def product_params
       params.require(:product).permit(:title, :description, :color)
